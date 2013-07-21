5696a5c35393eb77d2eb7c31532ecb6029c71f72
rgarcia <voodoorai2000@gmail.com>
2013-07-21 16:42:58 +0900

    scrapping previous legislation

--- lib/congress-scrapper.rb
+++ lib/congress-scrapper.rb
@@ -14,57 +14,60 @@ module Congress
     end
 
     def scrape
-      search_page = agent.get("http://www.congreso.es/portal/page/portal/Congreso/Congreso/Iniciativas/Busqueda%20Avanzada")
-      search_form = search_page.form_with(:action => /enviarCgiBuscadorAvIniciativas/)
-      search_form["TPTR"] = "Competencia Legislativa Plena"
-      results_page = search_form.submit
-
-      total_results = results_page.search("//*[contains(text(), 'Iniciativas encontradas')]/span").first.text.to_i
-      progress = ProgressBar.new("Scrapping", total_results)
-      
       proposals = []
+      legislations = {current_legislation: 'iw10', previous_legislation: 'iwi9'}
+      legislations.each do |legislation_name, legislation_value|
+        search_page = agent.get("http://www.congreso.es/portal/page/portal/Congreso/Congreso/Iniciativas/Busqueda%20Avanzada")
+        search_form = search_page.form_with(:action => /enviarCgiBuscadorAvIniciativas/)
+        search_form["TPTR"] = "Competencia Legislativa Plena"
+        search_form["BASE"] = legislation_value
+        results_page = search_form.submit
 
-      while results_page
-        results_page.search(".titulo_iniciativa a").each do |title|
-          @proposal_page = agent.get(title[:href])
+        total_results = results_page.search("//*[contains(text(), 'Iniciativas encontradas')]/span").first.text.to_i
+        progress = ProgressBar.new("Scrapping #{legislation_name}", total_results)
+
+        while results_page
+          results_page.search(".titulo_iniciativa a").each do |title|
+            @proposal_page = agent.get(title[:href])
           
-          proposal_type = clean_text(text_for(".subtitulo_competencias"))
+            proposal_type = clean_text(text_for(".subtitulo_competencias"))
 
-          status = clean_text(text_for("//*[@class='apartado_iniciativa' and contains(normalize-space(text()),'Resultado de la tramitación')]/following-sibling::*[@class='texto']")) ||
-                   clean_text(text_for("//*[@class='apartado_iniciativa' and contains(normalize-space(text()),'Situación actual')]/following-sibling::*[@class='texto']"))
+            status = clean_text(text_for("//*[@class='apartado_iniciativa' and contains(normalize-space(text()),'Resultado de la tramitación')]/following-sibling::*[@class='texto']")) ||
+                     clean_text(text_for("//*[@class='apartado_iniciativa' and contains(normalize-space(text()),'Situación actual')]/following-sibling::*[@class='texto']"))
 
-          commission_name = clean_text(text_for("//*[@class='apartado_iniciativa' and contains(normalize-space(text()),'Comisión competente:')]/following-sibling::*[@class='texto']"))
+            commission_name = clean_text(text_for("//*[@class='apartado_iniciativa' and contains(normalize-space(text()),'Comisión competente:')]/following-sibling::*[@class='texto']"))
 
-          proposer_name = clean_text(text_for("//*[@class='apartado_iniciativa' and contains(normalize-space(text()),'Autor:')]/following-sibling::*[@class='texto']"))
+            proposer_name = clean_text(text_for("//*[@class='apartado_iniciativa' and contains(normalize-space(text()),'Autor:')]/following-sibling::*[@class='texto']"))
 
-          proposed_at_text = text_for("//*[@class='texto' and contains(normalize-space(text()),'Presentado el')]")
-          proposed_at = Date.new($3.to_i, $2.to_i, $1.to_i) if proposed_at_text && proposed_at_text.match(/Presentado\s+el\s+(\d\d)\/(\d\d)\/(\d\d\d\d)/)
+            proposed_at_text = text_for("//*[@class='texto' and contains(normalize-space(text()),'Presentado el')]")
+            proposed_at = Date.new($3.to_i, $2.to_i, $1.to_i) if proposed_at_text && proposed_at_text.match(/Presentado\s+el\s+(\d\d)\/(\d\d)\/(\d\d\d\d)/)
 
-          closed_at_text = text_for("//*[@class='apartado_iniciativa' and contains(normalize-space(text()),'Tramitación seguida por la iniciativa:')]/following-sibling::*[@class='texto']")
-          closed_at = Date.new($3.to_i, $2.to_i, $1.to_i) if closed_at_text && closed_at_text.match(/Concluido\s+.+\s+desde (\d\d)\/(\d\d)\/(\d\d\d\d)/)
+            closed_at_text = text_for("//*[@class='apartado_iniciativa' and contains(normalize-space(text()),'Tramitación seguida por la iniciativa:')]/following-sibling::*[@class='texto']")
+            closed_at = Date.new($3.to_i, $2.to_i, $1.to_i) if closed_at_text && closed_at_text.match(/Concluido\s+.+\s+desde (\d\d)\/(\d\d)\/(\d\d\d\d)/)
           
-          body = full_proposal_text(@proposal_page)
+            body = full_proposal_text(@proposal_page)
 
-          proposal = {:title               => clean_text(title.content),
-                      :official_url        => "http://www.congreso.es" + title[:href],
-                      :proposal_type       => proposal_type,
-                      :closed_at           => closed_at,
-                      :status              => status,
-                      :category_name       => category(commission_name),
-                      :proposer_name       => proposer(proposer_name),
-                      :proposed_at         => proposed_at,
-                      :body                => body}
+            proposal = {:title               => clean_text(title.content),
+                        :official_url        => "http://www.congreso.es" + title[:href],
+                        :proposal_type       => proposal_type,
+                        :closed_at           => closed_at,
+                        :status              => status,
+                        :category_name       => category(commission_name),
+                        :proposer_name       => proposer(proposer_name),
+                        :proposed_at         => proposed_at,
+                        :body                => body}
  
-          progress.inc
+            progress.inc
           
-          proposals << proposal
-        end
+            proposals << proposal
+          end
         
-        next_page =  results_page.link_with(:text => /Siguiente/)
-        results_page = next_page.nil? ? nil : next_page.click
+          next_page =  results_page.link_with(:text => /Siguiente/)
+          results_page = next_page.nil? ? nil : next_page.click
+        end
+  
+        progress.finish
       end
-      
-      progress.finish
       
       proposals
     end
--- spec/lib/scrapper_spec.rb
+++ spec/lib/scrapper_spec.rb
@@ -20,22 +20,12 @@ describe Congress::Scrapper do
     
     it "should go to the proposal search form" do
       Congress::Scrapper.scrape
-      a_request(:get, search_page).should have_been_made
+      a_request(:get, search_page).should have_been_made.times(2)
     end
     
     it "should search the proposals we're interested in" do
       Congress::Scrapper.scrape
-      a_request(:post, search_results_page).with{|r| r.body =~ /TPTR=Competencia\+Legislativa\+Plena/}.should have_been_made
-    end
-    
-    it "should create one proposal for record found" do
-      proposals = Congress::Scrapper.scrape
-      proposals.collect { |p| p[:title] }.should == [
-        "Proyecto de Ley de almacenamiento geológico de dióxido de carbono",
-        "Proyecto de Ley del régimen de cesión de tributos del Estado a la Comunitat Valenciana y de fijación del alcance y condiciones de dicha cesión",
-        "Proyecto de Ley de Reforma del Sistema de Apoyo Financiero a la Internacionalización de la empresa española",
-        "Proyecto de Ley por la que se modifica el Estatuto Legal del Consorcio de Compensación de Seguros, aprobado por el Real Decreto Legislativo 7/2004, de 29 de octubre, para suprimir las funciones del Consorcio de Compensación de Seguros en relación con los seguros obligatorios de viajeros y del cazador y reducir el recargo destinado a financiar las funciones de liquidación de entidades aseguradoras, y el texto refundido de la Ley de Ordenación y Supervisión de los Seguros Privados, aprobado por el Real Decreto Legislativo 6/2004, de 29 de octubre"
-      ]
+      a_request(:post, search_results_page).with{|r| r.body =~ /TPTR=Competencia\+Legislativa\+Plena/}.should have_been_made.times(2)
     end
     
     it "should populate open proposals info" do
